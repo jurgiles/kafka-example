75d428ef517720d51aa8bd0deb7550cbdf777547
Johny Urgiles <jurgiles@jurgiles.local>
2018-05-10 15:28:38 -0400

    Start setting up a second broker configuration

--- status-kafka-zookeeper.sh
+++ status-kafka-zookeeper.sh
@@ -4,3 +4,7 @@ source kafka-sample.env
 
 kafka-topics.sh --list --zookeeper localhost:2181
 
+
+zookeeper-shell.sh localhost:2181 <<< "ls /brokers/ids"
+
+
--- stop-kafka-zookeeper.sh
+++ stop-kafka-zookeeper.sh
@@ -4,6 +4,7 @@ source kafka-sample.env
 
 zookeeper-server-stop.sh $KAFKA_PATH/config/zookeeper.properties
 
-kafka-server-stop.sh $KAFKA_PATH/config/server.properties
+kafka-server-stop.sh $S_CONFIG/server-uno.properties
+kafka-server-stop.sh $S_CONFIG/server-dos.properties
 
 
