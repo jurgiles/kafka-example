848933f19a3fb0e6c232fa92cd606c7175dd1ea9
Johny Urgiles <jurgiles@jurgiles.local>
2018-05-11 15:57:43 -0400

    Correct replicator and worker configs

--- confluent-multicluster/consumer-othergroup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source config.env
-
-TOPIC=$1
-
-kafka-console-consumer  --consumer-property "client.id=othergroup-consumer-$RANDOM" --bootstrap-server localhost:9092 --group othergroup --topic $TOPIC
--- confluent-multicluster/tools-offset-consumer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# View contents of the Kafka __consumer_offsets topic
-
-source config.env
-
-kafka-console-consumer --bootstrap-server localhost:9092 --topic __consumer_offsets --formatter "kafka.coordinator.group.GroupMetadataManager\$OffsetsMessageFormatter"
