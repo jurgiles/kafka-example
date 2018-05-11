971bc5286cffc56e2d6c5506077178c5252db6fe
Johny Urgiles <jurgiles@jurgiles.local>
2018-05-11 17:01:16 -0400

    When starting replicator make sure to update plugin path with confluent binarODy path

--- confluent-multicluster/start-replicator.sh
+++ confluent-multicluster/start-replicator.sh
@@ -2,5 +2,9 @@
 
 source config.env
 
+PLUGIN_JAR_PATH=$KAFKA_PATH/share/java/
+
+sed -i '' -e "s|^plugin.path=.*|plugin.path=$PLUGIN_JAR_PATH|g" $S_CONFIG/connect-standalone.properties
+
 connect-standalone $S_CONFIG/connect-standalone.properties $S_CONFIG/quickstart-replicator.properties
 
