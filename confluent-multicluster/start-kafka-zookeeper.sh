10de9905996faaa15062283cd03031e691a44357
Johny Urgiles <jurgiles@jurgiles.local>
2018-05-11 14:55:38 -0400

    update configs with correct ports

--- confluent-multicluster/start-kafka-zookeeper.sh
+++ confluent-multicluster/start-kafka-zookeeper.sh
@@ -2,8 +2,14 @@
 
 source config.env
 
+echo 'Starting zookeepers'
 zookeeper-server-start -daemon $S_CONFIG/zookeeper-src.properties
+zookeeper-server-start -daemon $S_CONFIG/zookeeper-dest.properties
 
+sleep 1
+
+echo 'Starting kafkas'
 kafka-server-start -daemon $S_CONFIG/server-src.properties
+kafka-server-start -daemon $S_CONFIG/server-dest.properties
 
 
